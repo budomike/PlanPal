51c883bdb463200a6705ec277fea484b3f6065d3
budomike <89367969+budomike@users.noreply.github.com>
2024-05-27 13:40:20 -0700

    Update edit-form.tsx

--- app/ui/events/edit-form.tsx
+++ app/ui/events/edit-form.tsx
@@ -35,6 +35,7 @@ export default function EditEventForm({
               type="text"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={event.title}
+              required
             />
 
             <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
@@ -78,6 +79,7 @@ export default function EditEventForm({
                 type="date"
                 defaultValue={new Date(event.date).toISOString().split('T')[0]}
                 className="peer block rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
               />
             </div>
           </div>
@@ -97,6 +99,7 @@ export default function EditEventForm({
                 type="time"
                 defaultValue={event.time}
                 className="peer block rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
               />
             </div>
           </div>
