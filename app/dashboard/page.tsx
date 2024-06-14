331ccb6035398178c20446942520be8ca365cd9e
budomike <89367969+budomike@users.noreply.github.com>
2024-06-14 15:27:27 -0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,27 @@
-
 import UpcomingEvent from '@/app/ui/dashboard/upcoming-events';
 import PastEvent from '../ui/dashboard/past-events';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchUpcomingEvents, fetchPastEvents } from '../lib/data';
- 
+
 export default async function Page() {
   const upcomingEvents = await fetchUpcomingEvents();
   const pastEvents = await fetchPastEvents();
+  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-10">
-        <UpcomingEvent upcomingEvent={upcomingEvents} />
-        <div className="border-l border-gray-300 h-full"></div>
-        <PastEvent pastEvent={pastEvents} />
+      <div className="mt-6 flex flex-col md:flex-row">
+        <div className="flex-grow md:w-1/2">
+          <UpcomingEvent upcomingEvent={upcomingEvents} />
+        </div>
+        <div className="flex items-center justify-center md:mx-4 md:w-1">
+          <div className="h-full w-px bg-gray-300"></div>
+        </div>
+        <div className="flex-grow md:w-1/2">
+          <PastEvent pastEvent={pastEvents} />
+        </div>
       </div>
     </main>
   );
