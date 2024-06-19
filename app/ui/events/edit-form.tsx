eeae092f183152218f1a427a0758bf698cb1df0d
budomike <89367969+budomike@users.noreply.github.com>
2024-06-19 11:06:10 -0700

    Limit length on event titles and descriptions

--- app/ui/events/edit-form.tsx
+++ app/ui/events/edit-form.tsx
@@ -36,6 +36,7 @@ export default function EditEventForm({
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={event.title}
               required
+              maxLength={40}
             />
 
             <InformationCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
@@ -60,6 +61,7 @@ export default function EditEventForm({
                 defaultValue={event.description}
                 placeholder="Enter a description"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                maxLength={150}
               />
               <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
