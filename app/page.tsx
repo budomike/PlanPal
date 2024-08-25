e11f25d1d3c6ebdae09150e4d11558d069dbfd03
budomike <89367969+budomike@users.noreply.github.com>
2024-08-25 09:46:45 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-96 text-white text-2xl font-bold md:w-40 md:text-4xl">
-            PenPal
+            PlanPal
           </div>
         </div>
         <LoginForm />
